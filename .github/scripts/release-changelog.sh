b1d3ab241e3551a7d1db6bcd4fab56fe54febcbb
Cameron Clough <cameronjclough@gmail.com>
2026-08-08 02:51:59 +0100

    fix(release): include all commits in nightly changelogs (#38)
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- .github/scripts/release-changelog.sh
+++ .github/scripts/release-changelog.sh
@@ -1,25 +1,5 @@
 #!/usr/bin/env bash
 
-is_ci_release_commit() {
-  local subject="$1"
-  local revert_subject='^[rR][eE][vV][eE][rR][tT][[:space:]]+"(.*)"([[:space:]]+\(#[0-9]+\))?$'
-  local conventional_subject='^([[:alpha:]][[:alnum:]-]*)(\(([^)]*)\))?!?:'
-  local ci_type='^[cC][iI]$'
-  local ci_scope='(^|[[:space:]]*,[[:space:]]*)[cC][iI]([_-][[:alnum:]_-]+)?([[:space:]]*,[[:space:]]*|$)'
-
-  while [[ "$subject" =~ $revert_subject ]]; do
-    subject="${BASH_REMATCH[1]}"
-  done
-
-  if [[ ! "$subject" =~ $conventional_subject ]]; then
-    return 1
-  fi
-
-  local commit_type="${BASH_REMATCH[1]}"
-  local commit_scope="${BASH_REMATCH[3]}"
-  [[ "$commit_type" =~ $ci_type || "$commit_scope" =~ $ci_scope ]]
-}
-
 list_fork_release_commits() {
   local previous_release_ref="$1"
   local fork_source_ref="$2"
